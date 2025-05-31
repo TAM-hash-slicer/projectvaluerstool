d0a2f9f1265d3eaf53265932dd3b76f47dea9135
vansjavier <ivanjavier34@gmail.com>
2025-05-31 13:01:23 +0700

    push

--- src/main/java/pendekatan_Pasar/model/Subjek.java
+++ src/main/java/pendekatan_Pasar/model/Subjek.java
@@ -9,4 +9,28 @@ public class Subjek {
     protected int yearBuilt;
     protected double transactionPrice;
     protected String transactionDate;
+
+    public void setId(String id) {
+    }
+
+    public void setType(String residensial) {
+    }
+
+    public void setLocation(String jakarta) {
+    }
+
+    public void setLandArea(double i) {
+    }
+
+    public void setBuildingArea(double i) {
+    }
+
+    public void setYearBuilt(int i) {
+    }
+
+    public void setTransactionPrice(double i) {
+    }
+
+    public void setTransactionDate(Object o) {
+    }
 }
