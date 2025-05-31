fdd80a33f35c1c8dd4f16beb26856eee001f5a96
vansjavier <ivanjavier34@gmail.com>
2025-05-31 12:23:45 +0700

    push

---
+++ src/main/java/pendekatan_Pasar/model/Pembanding.java
@@ -0,0 +1,6 @@
+package pendekatan_Pasar.model;
+
+public class Pembanding extends Subjek {
+    private double adjustmentValue;
+    private String adjustmentNotes;
+}
---
+++ src/main/java/pendekatan_Pasar/model/Subjek.java
@@ -0,0 +1,12 @@
+package pendekatan_Pasar.model;
+
+public class Subjek {
+    protected String id;
+    protected String type;
+    protected String location;
+    protected double landArea;
+    protected double buildingArea;
+    protected int yearBuilt;
+    protected double transactionPrice;
+    protected String transactionDate;
+}
